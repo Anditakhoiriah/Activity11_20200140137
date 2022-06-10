83199fbe1455d45438abb979a3be96675f2a6c8f
AnditaKhoiriah <andita.khoiriah.ft20@mail.umy.ac.id>
2022-06-11 00:56:05 +0700

    Initial commit 1

---
+++ app/src/main/java/com/example/todoapp/model/User.java
@@ -0,0 +1,40 @@
+package com.example.todoapp.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class User extends AppCompatActivity {
+    private String id, name, email;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
